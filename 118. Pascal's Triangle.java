cc26b0832a9f78adc8c2736a6a542f42f1e1a8f1
Yashvardhan Singh Bhadauria <yashsingh121@gmail.com>
2022-02-03 13:32:21 +0530

    Update 118. Pascal's Triangle.java

--- 118. Pascal's Triangle.java
+++ 118. Pascal's Triangle.java
@@ -1,17 +1,25 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-      List<List<Integer>> list1 = new ArrayList();
-        for(int i =0; i< numRows;i++){
-            List<Integer> list2 = new ArrayList();
-            for(int j=0; j<=i; j++){
-                if(j == 0 || j == i){
-                    list2.add(1);
-                }else{
-                    list2.add(list1.get(i-1).get(j-1) + list1.get(i-1).get(j));
-                }
-            }
-            list1.add(list2);
+        List<List<Integer>> ans = new ArrayList<>();
+        if(numRows==0){
+            return ans;
         }
-        return list1;
+        List<Integer> firstrow = new ArrayList();
+        firstrow.add(1);
+        ans.add(firstrow);
+        
+        for(int i=1;i<numRows;i++){
+             List<Integer> prevRow = ans.get(i-1);
+              List<Integer> Row = new ArrayList();
+            
+            Row.add(1);
+            
+            for(int j=1;j<i;j++){
+                Row.add(prevRow.get(j-1)+ prevRow.get(j));
+            }
+            Row.add(1);
+            ans.add(Row);
+        }
+       return ans;
     }
 }
