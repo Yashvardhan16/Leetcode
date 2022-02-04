739ec409656414b19bc75d88a757b5b67a8df56a
Yashvardhan Singh Bhadauria <yashsingh121@gmail.com>
2022-02-04 12:39:35 +0530

    Update 179. Largest Number.java

--- 179. Largest Number.java
+++ 179. Largest Number.java
@@ -1,27 +1,20 @@
 class Solution {
-    public String largestNumber(int[] nums) {
-         String[] sArr = new String[nums.length];
-        for(int i=0;i<nums.length;i++){
-            sArr[i] = Integer.toString(nums[i]);
+    public String largestNumber(int[] nums) {   
+        if(nums.length==0)
+            return "";
+        String[] ans=new String[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            ans[i]=Integer.toString(nums[i]);
         }
-        Arrays.sort(sArr,new Comparator<String>(){
-            public int compare(String a,String b){
-                String s1 = a + b;
-                String s2 = b + a;
-                return s2.compareTo(s1);
-            }
-        });
-
-        if(sArr[0].equals("0")){
+        Arrays.sort(ans,(a,b) -> (b+a).compareTo(a+b));
+        StringBuilder res=new StringBuilder();
+        if(ans[0].equals("0"))
             return "0";
+        for(int i=0;i<ans.length;i++)
+        {
+            res.append(ans[i]);
         }
-    
-        StringBuilder sb = new StringBuilder();
-        for(String s : sArr){
-            sb.append(s);
-        }
-         
-        return sb.toString();
+        return res.toString();
     }
 }
-    
