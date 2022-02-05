ca6fb6639ed21ddb473db95e0a9d45d219e50c57
Yashvardhan Singh Bhadauria <yashsingh121@gmail.com>
2022-02-05 14:07:39 +0530

    Update 28. Implement strStr().java

--- 28. Implement strStr().java
+++ 28. Implement strStr().java
@@ -1,10 +1,34 @@
+//Solution 1
+
 class Solution {
     public int strStr(String haystack, String needle) {
-        if(!haystack.contains(needle)){
-            return -1;
-        } int index =0;
-         if(haystack.contains(needle)){
-            index= haystack.indexOf(needle) ;
+        int n = haystack.length();
+         int m = needle.length();
+          for(int i=0;i<=n-m;i++){
+              int j=0;
+           for( j=0;j<m;j++){
+            if(needle.charAt(j)==haystack.charAt(j+i)){
+                continue;
+            }
+         else{
+             break;
+         }
+        }if(j==needle.length()){
+               return i;
+           }
+          }
+        return -1;
+    }
+}
+
+//Solution 2
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(haystack.length()==0 && needle.length()==0) return 0;
+        if(haystack.contains(needle)){
+            return haystack.indexOf(needle);
         }
-        return index;
-}}
+        return -1;
+    }
+}
