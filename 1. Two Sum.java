25196318da8983f31f7c61dc1d0e52bb7a0645a4
Yashvardhan Singh Bhadauria <yashsingh121@gmail.com>
2022-02-08 18:34:10 +0530

    Update 1. Two Sum.java

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -1,16 +1,32 @@
-class Solution{
-public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+//HashMap
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap();
+        int[] ans = new int[2];
+       for(int i=0;i<nums.length;i++){
+          if(map.containsKey(target-nums[i])){
+              ans[0] = i;
+              ans[1] = map.get(target-nums[i]);
+          }
+           map.put(nums[i],i);
+       }
+            return ans;
         
-        for(int i=0, j=0; i<nums.length; i++) {
-            int n = target - nums[i];
-            
-            if(map.containsKey(n))
-                if(map.get(n) != i)
-                    return new int[] {i, map.get(n)};
-            
-            map.put(nums[i], i);
-        }
-        
-        return new int[] {};
-    }}
+    }
+}
+
+//BruteForce
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+       for(int i=0;i<nums.length;i++){
+         for(int j=i+1;j<nums.length;j++){
+         if(nums[i]+nums[j]==target){
+             
+             ans[0] =i;
+             ans[1] =j;
+             }
+            }}
+            return ans;
+          }
+}
