d50736fec4dcc2c968ff3332572e27247cc58bdc
Yashvardhan Singh Bhadauria <yashsingh121@gmail.com>
2022-02-20 14:20:34 +0530

    Update 416. Partition Equal Subset Sum.java

--- 416. Partition Equal Subset Sum.java
+++ 416. Partition Equal Subset Sum.java
@@ -1,27 +1,23 @@
 class Solution {
-public boolean canPartition(int[] nums) {
-
-    int sum = 0;
-    for(int value:nums)
-        sum += value;
-    
-    if(sum%2 != 0)return false;
-    
-    HashMap<Integer,Boolean> map = new HashMap<>();
-    
-    return isPossible(nums,sum/2,0,map);
-}
-
- boolean isPossible(int nums[],int total,int index,HashMap<Integer,Boolean> map){
-    if(map.containsKey(total))return map.get(total);
-    if(total == 0)return true;
-    if(total < 0)return false;
-    
-    for(int i=index;i<nums.length;i++){
-        map.put(total, isPossible(nums,total-nums[i],i+1,map));
-        if(map.get(total))return true;
+    public boolean canPartition(int[] nums) {
+        int sum=0;
+        for(int i:nums){
+            sum+=i;
+        }
+        if(sum%2==1){
+            return false;
+        }
+            sum = sum/2;
+        
+        boolean[] dp = new boolean[sum+1];
+        dp[0] = true;
+        
+        for(int i:nums){
+            for(int j=sum;j>=i;j--){
+                dp[j] = dp[j] | dp[j-i];
+            }
+        }
+        return dp[sum];
+         
     }
-    
-    return false;
-}
 }
