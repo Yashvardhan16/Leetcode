db8effd5a1592fb0ae8754a8852b04d95a2668a2
Yashvardhan Singh Bhadauria <yashsingh121@gmail.com>
2022-02-21 08:44:36 +0530

    Update 119. Pascal's Triangle II.java

--- 119. Pascal's Triangle II.java
+++ 119. Pascal's Triangle II.java
@@ -1,12 +1,15 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-    List<Integer> list = new ArrayList<Integer>();
-    for(int i = 0;i<rowIndex+1;i++) {
-    		list.add(1);
-    		for(int j=i-1;j>0;j--) {
-    			list.set(j, list.get(j-1)+list.get(j));
-    		}
+        List<Integer> ans = new ArrayList();
+        if(rowIndex<0) return ans;
+        
+        ans.add(1);
+        for(int i=1;i<rowIndex+1;i++){
+            for(int j= ans.size()-1;j>0;j--){
+                ans.set(j,ans.get(j-1)+ans.get(j));
+            }
+            ans.add(1);
+        }
+  return ans;
     }
-    return list;
-}
 }
