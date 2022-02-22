7fe78218c0e03707e04e79d5ff586195b6592758
Yashvardhan Singh Bhadauria <yashsingh121@gmail.com>
2022-02-22 18:39:40 +0530

    Create 9. Palindrome Number.java

---
+++ 9. Palindrome Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int n) {
+         int ans=0;
+        int temp=n;
+        while(n>0){
+            ans=ans*10+n%10;
+            n/=10;
+        }
+        if(ans==temp){
+            return true;
+        }
+        return false;
+    }
+}
