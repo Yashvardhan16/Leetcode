9ebba6fc1cd5b9a7512502c0097066ca921656cc
Yashvardhan Singh Bhadauria <yashsingh121@gmail.com>
2022-02-22 15:21:08 +0530

    Update 415. Add Strings.java

--- 415. Add Strings.java
+++ 415. Add Strings.java
@@ -1,35 +1,18 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        
+        StringBuilder sb = new StringBuilder();
         int i = num1.length() -1;
         int j = num2.length() -1;
-        
-        StringBuilder sb = new StringBuilder();
-        int carry = 0;
-        
-        while(i >= 0 || j >= 0 || carry > 0){
-            int n1 = 0;
-            int n2 = 0;
-            
-            if(i >= 0){
-                n1 = num1.charAt(i) - '0';
-                i--;
-            }
-            if(j >= 0){
-                n2 = num2.charAt(j) - '0';
-                j--;
-            }
-            
-            int num = (n1 + n2 + carry) % 10;
-            carry = (n1 + n2 + carry) >= 10 ? 1 : 0;
-            sb.append(num);
-           
+        int carry=0;
+        while(i>=0||j>=0||carry!=0){
+           int sum=carry;
+            if(i>=0) sum+=num1.charAt(i--)-'0';
+            if(j>=0) sum+=num2.charAt(j--)-'0';
+            sb.insert(0,sum%10);
+          carry=sum/10;
+          
         }
-
-        
-        return sb.reverse().toString();
-        
-        
-        
+        if(carry>0) sb.insert(0,1);
+        return sb.toString();
     }
 }
