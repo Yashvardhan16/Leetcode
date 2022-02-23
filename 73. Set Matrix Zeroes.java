8500d3e6a580face81fef41af061f89070066cf1
Yashvardhan Singh Bhadauria <yashsingh121@gmail.com>
2022-02-23 09:53:23 +0530

    Create 73. Set Matrix Zeroes.java

---
+++ 73. Set Matrix Zeroes.java
@@ -0,0 +1,29 @@
+//Space Complexity O(mn)
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[] rowZero = new int[n];
+         int[] ColZero = new int[m];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(matrix[i][j]==0){
+                    rowZero[i]=1;
+                    ColZero[j] = 1;
+                }
+            }
+        }
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(rowZero[i]==1||ColZero[j]==1){
+                    matrix[i][j]=0;
+                    
+                }
+    }
+}
+        
+    }}
