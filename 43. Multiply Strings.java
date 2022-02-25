62093285ad0eaf591e548a88304a9b3bfa500273
Yashvardhan Singh Bhadauria <yashsingh121@gmail.com>
2022-02-25 09:56:23 +0530

    Update 43. Multiply Strings.java

--- 43. Multiply Strings.java
+++ 43. Multiply Strings.java
@@ -1,27 +1,31 @@
-class Solution {
-	public String multiply(String num1, String num2) {
-		if ("0".equals(num1) || "0".equals(num2))
-			return "0";
-
-		int[] ans = new int[num1.length() + num2.length() - 1];
-
-		for (int i = 0; i < num1.length(); i++) {
-			for (int j = 0; j < num2.length(); j++) {
-				ans[i + j] += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-			}
-		}
-
-		for (int i = ans.length - 1; i > 0; i--) {
-			ans[i - 1] += ans[i] / 10;
-			ans[i] %= 10;
-		}
-
-		StringBuilder sb = new StringBuilder();
-		for (int i : ans) {
-			sb.append(i);
-		}
-
-		return sb.toString();
-	}
-
+ StringBuilder sb = new StringBuilder();
+        int n = num1.length();
+        int m = num2.length();
+        
+        if(n==0||m==0||"0".equals(num1)||"0".equals(num2)){
+            return "0";
+        }
+        if("1".equals(num1)) return num2;
+        if("1".equals(num2)) return num1;
+        
+        int[] ans = new int[n+m];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int product=(num1.charAt(i)-'0')*(num2.charAt(j)-'0');
+                
+                product+= ans[i+j+1];
+                ans[i+j+1] = product%10;
+                ans[i+j] += product/10;
+                
+            }
+            }
+            for(int z:ans){
+                if(z==0&& sb.length()==0){
+                    continue;
+                }
+                sb.append(z);
+            }
+            return sb.toString();
+        
+    }
 }
