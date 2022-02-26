0f6507c01e931c782677d6b393739459f1962a94
Yashvardhan Singh Bhadauria <yashsingh121@gmail.com>
2022-02-26 09:18:34 +0530

    Update 240. Search a 2D Matrix II.java

--- 240. Search a 2D Matrix II.java
+++ 240. Search a 2D Matrix II.java
@@ -1,19 +1,23 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
- if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+        if(matrix.length==0||matrix==null){
+            return false;
+        }
+
+        int row =0;
+        int col = matrix[0].length-1;
+        while(row<matrix.length && col>=0){
+            if(matrix[row][col]==target){
+               return true;
+                
+            }
+           else if(matrix[row][col]>target){
+                col--;
+                
+            }else if(matrix[row][col]<target){
+                row++;
+            }
+        }
         return false;
     }
-    int row = 0;
-    int col = matrix[0].length - 1;
-    while (row < matrix.length && col >= 0) {
-        if (matrix[row][col] == target) {
-            return true;
-        } else if (matrix[row][col] < target) {
-            row++;
-        } else {
-            col--;
-        }
-    }
-    return false;
-}
 }
