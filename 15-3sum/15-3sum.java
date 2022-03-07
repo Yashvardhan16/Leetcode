eaf5c0c65bf5ef7e91f63946201556e8b76c9ed1
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-07 09:01:07 +0530

    Time: 1015 ms (9.81%), Space: 119.6 MB (17.29%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,9 +1,6 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Set<List<Integer>> set = new HashSet<>();
-        if(nums.length==0){
-            return new ArrayList<>(set);
-        }
         Arrays.sort(nums);
        for(int i=0;i<nums.length-2;i++){
            int j=i+1;
