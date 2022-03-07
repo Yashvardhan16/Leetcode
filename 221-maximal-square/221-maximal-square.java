0fed1836214846a2ff0e9fc221d72ce02beb22fd
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-07 16:43:10 +0530

    Time: 10 ms (42.35%), Space: 57.7 MB (40.17%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -9,8 +9,9 @@ class Solution {
             for(int j=1;j<=m;j++){
                 if(matrix[i-1][j-1]=='1'){
                     dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
+                    ans = Math.max(ans,dp[i][j]);
                 }
-                ans = Math.max(ans,dp[i][j]);
+                
             }
         }
     return ans*ans;
