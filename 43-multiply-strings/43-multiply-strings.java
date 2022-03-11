966f9c67234df66597037b1557362083710c6935
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-11 21:23:28 +0530

    Time: 5 ms (86.25%), Space: 42.6 MB (60.99%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int n = num1.length();
+        int m = num2.length();
+        
+        if(n==0||m==0||"0".equals(num1)||"0".equals(num2)){
+            return "0";
+        }
+        if("1".equals(num1)) return num2;
+        if("1".equals(num2)) return num1;
+        
+        int[] ans = new int[n+m];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int product=(num1.charAt(i)-'0')*(num2.charAt(j)-'0');
+                
+                product+= ans[i+j+1];
+                ans[i+j+1] = product%10;
+                ans[i+j] += product/10;
+                
+            }
+            }
+            for(int z:ans){
+                if(z==0&& sb.length()==0){
+                    continue;
+                }
+                sb.append(z);
+            }
+            return sb.toString();
+        
+    }
+}
