58905a487dd2ffb32c280def490c02cd458edc89
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-12 11:23:35 +0530

    Time: 2 ms (81.03%), Space: 42.4 MB (13.13%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        // if(s==null||s.length()==1||s.length()%2==1) return false;
+        
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='('||c=='['||c=='{'){
+                st.push(c);
+            }
+                else if(st.empty()) return false;
+            else if(c==')' && st.pop()!='('){
+                  return false;
+            }
+            
+            else if(c==']' && st.pop()!='['){
+                return false;
+            }
+            else if(c=='}' && st.pop()!='{'){
+                  return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
