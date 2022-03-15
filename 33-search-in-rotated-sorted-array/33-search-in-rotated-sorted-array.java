9cf95693094c6287b02894fd94287c602000da6d
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-15 19:38:11 +0530

    Time: 1 ms (67.66%), Space: 42.6 MB (41.46%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,36 +1,33 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int lo=0;
-        int hi = nums.length-1;
-        
+        int lo =0;int hi = nums.length-1;
         while(lo<hi){
-            int mid = lo +(hi-lo)/2;
-            if(nums[mid]>nums[hi]){
-                lo=mid+1;;
+            int mid = lo+(hi-lo)/2;
+            if(nums[mid]==target) {
+                return mid;
+            }if(nums[mid]>nums[hi]){
+                lo=mid+1;
             }else{
-                hi=mid;
+                hi =mid;
             }
         }
-        int start = lo;
-        lo =0;
-        hi=nums.length-1;
+        int start =lo;
+         lo = 0;
+         hi = nums.length-1;
         if(target>=nums[start] && target<=nums[hi]){
-            lo=start;
+            lo = start;
         }else{
-            hi = start;
+            hi =start;
         }
-          while(lo<=hi){
-              int mid = lo +(hi-lo)/2;
-              if(nums[mid]==target){
-                  return mid;
-              }else if(nums[mid]<target){
-                  lo=mid+1;
-              }else {
-                  hi=mid-1;
-              }
-          }  
+        while(lo<=hi){
+            int mid = lo+(hi-lo)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target){
+                lo=mid+1;
+            }else{
+                hi=mid-1;
+            }
+        }
         return -1;
-        
-        }
     }
-    
+}
