bec7801365f1a70de2ef4c6abacfb021c4157bbb
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-15 21:34:48 +0530

    Time: 0 ms (100.00%), Space: 43 MB (29.54%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -2,7 +2,6 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        //transpose matrix
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
                 int temp = matrix[i][j];
@@ -10,16 +9,21 @@ class Solution {
                 matrix[j][i] = temp;
             }
         }
+        //123 147
+        //456 258
+        //789 369
+        
         for(int i=0;i<n;i++){
-        //reverse matrix
-        int left =0;
-        int right = matrix[0].length-1;
-        while(left<right){
-            int temp = matrix[i][left];
-            matrix[i][left] = matrix[i][right];
-            matrix[i][right] = temp;
-            left++;right--;
-        }
-        }
+            int left =0;
+            int right = matrix.length-1;
+               while(left<right){
+                   int temp = matrix[i][left];
+                   matrix[i][left] = matrix[i][right];
+                   matrix[i][right] = temp;
+                   left++;right--;
+                   
+               }
+        
+    }
     }
 }
