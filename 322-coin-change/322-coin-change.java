d93549086a6e1d143ba7ae24e9c178e8fd192b80
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-16 08:53:08 +0530

    Time: 15 ms (87.31%), Space: 45 MB (52.30%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,5 +1,6 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
         int[] dp = new int[amount+1];
         Arrays.fill(dp,amount+1);
         dp[0] =0;
@@ -8,6 +9,6 @@ class Solution {
                 dp[i] = Math.min(dp[i],dp[i-coin]+1);
             }
         }
-        return dp[amount]<=amount ? dp[amount]:-1;
+        return dp[amount]<=amount?dp[amount]:-1;
     }
 }
