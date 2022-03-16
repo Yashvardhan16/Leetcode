7ddf56f9f06a2c2ebf41aaa1a9b1f6f42260211f
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-16 08:49:03 +0530

    Time: 5 ms (43.81%), Space: 45.1 MB (48.27%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        if(pushed.length!=popped.length) return false;
          Stack<Integer> st = new Stack();
         int i=0;
         for(int val:pushed){
