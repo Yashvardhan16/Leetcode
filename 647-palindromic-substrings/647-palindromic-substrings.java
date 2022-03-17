ee402026bedc11566db15403be7fbb0962c6bfc1
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-17 18:47:49 +0530

    Time: 5 ms (79.76%), Space: 41.9 MB (61.64%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,16 +1,15 @@
 class Solution {
    
     public int countSubstrings(String s) {
-              if(s==null || s.isEmpty()) return 0;
+            if(s==null || s.length()==0) return 0;
         int count = 0;
         for(int i=0; i<s.length(); i++){
-            count += extendPalindrom(s, i, i) + extendPalindrom(s, i, i+1);
+            count += helper(s, i, i) + helper(s, i, i+1);
         }
         return count;
     }
-    
-    private int extendPalindrom(String s, int left, int right){
-        int count = 0;
+  int  helper(String s,int left,int right){
+       int count =0;
         while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
             count++;
             left--;
