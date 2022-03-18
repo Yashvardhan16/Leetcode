3409d2fb22752b820ca568a428c9a63ec400e4d5
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-18 14:57:00 +0530

    Time: 101 ms (88.01%), Space: 40.4 MB (75.85%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,16 +1,14 @@
 class Solution {
     boolean[][] visited;
     public boolean exist(char[][] board, String word) {
-     visited = new boolean[board.length][board[0].length];
-        
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                if((word.charAt(0) == board[i][j]) && helper(board, word, i, j, 0)){
+        visited = new boolean[board.length][board[0].length];
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(word.charAt(0)==board[i][j] && helper(board,word,i,j,0)){
                     return true;
                 }
             }
         }
-        
         return false;
     }
  boolean   helper(char[][] board , String word , int i,int j,int index){
