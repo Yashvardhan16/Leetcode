b37dc2d165c477faf91477a720a30b68aa01e326
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-19 12:14:49 +0530

    Time: 4 ms (79.62%), Space: 43.4 MB (61.98%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n = s.length(); int m = t.length();
+        if(n!=m) return false;
+        int[] chars = new int[256];
+        
+        for(int i=0;i<n;i++){
+            chars[s.charAt(i)]++;
+            chars[t.charAt(i)]--;
+        }
+        
+        for(int i:chars){
+            if(i!=0) return false;
+        }
+        return true;
+    }
+}
