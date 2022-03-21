7151b2ab1a6166f6b4c24b64faaf789cac834a57
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-21 17:40:05 +0530

    Added solution - LeetHub

---
+++ Subarray with given sum - GFG/subarray-with-given-sum.java
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int s = sc.nextInt();
+
+            int[] m = new int[n];
+            for (int j = 0; j < n; j++) {
+                m[j] = sc.nextInt();
+            }
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(m, n, s);
+            for(int ii = 0;ii<res.size();ii++)
+                System.out.print(res.get(ii) + " ");
+            System.out.println();
+        }
+    }
+
+}// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        // Your code here
+        ArrayList<Integer> list = new ArrayList<>();
+       int left = 0;
+       int right =0;
+       int sum =0;
+       while(right<n){
+           sum += arr[right];
+           while(sum>s){
+               sum -= arr[left];
+               left++;
+           }
+           
+           if(sum==s){
+               list.add(left+1);
+               list.add(right+1);
+               return list;
+           }
+           right++;
+       }
+         list.add(-1);
+         return list;
+    }
+}
