cdf990ca474a1cabd2202eee3c4263649a2949dc
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-21 11:15:12 +0530

    Time: 12 ms (13.31%), Space: 43.9 MB (46.50%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+          List<List<Integer>> list = new LinkedList<>();
+        helper(candidates,0,new ArrayList<>(),target,list);
+        return list;
+    }
+   void helper(int[] candidates,int start,List<Integer> ans,int target,List<List<Integer>> list){
+         if(target<0) return;
+       if(target==0)  list.add(new ArrayList<>(ans));
+      
+        for(int i=start;i<candidates.length;i++){
+            if(i > start && candidates[i] == candidates[i-1]) continue;
+            ans.add(candidates[i]);
+            helper(candidates,i+1,ans,target-candidates[i],list);
+            ans.remove(ans.size()-1);
+        }
+    }
+}
