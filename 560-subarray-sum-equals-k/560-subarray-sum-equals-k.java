4596b6947414dfc0b073fcc4927afed140bf362e
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-21 17:51:36 +0530

    Time: 30 ms (70.94%), Space: 63.3 MB (6.24%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -6,10 +6,10 @@ class Solution {
         map.put(0,1);
         for(int i=0;i<nums.length;i++){
             sum += nums[i];
-            if(map.containsKey(sum-k))
-                count += map.get(sum-k);                                      map.put(sum,map.getOrDefault(sum,0)+1);
-            
-        }
+            if(map.containsKey(sum-k)){
+                count += map.get(sum-k);
+            } map.put(sum,map.getOrDefault(sum,0)+1);
+             }
         return count;
     }
 }
