ba6a4076c544a414209fae6a4c2ef9b812a2fe1c
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-03-23 12:25:06 +0530

    Time: 2 ms (64.34%), Space: 53.9 MB (55.87%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -12,10 +12,13 @@ class Solution {
    
     
     void dfs(int[][] isConnected,int i){
+        //make value 1 to 0
         isConnected[i][i]=0;
         for(int j=0;j<isConnected.length;j++){
             if(isConnected[i][j]==1){
+                // mark 1 as 0
                 isConnected[i][j] =0;
+                //if already visited check by using dfs function
             if(isConnected[j][j]==1){
                 dfs(isConnected,j);
             }
