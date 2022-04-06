6fea570ff8f4b7a14b54f368e2efe56e7b88b785
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-04-06 11:38:54 +0530

    Time: 165 ms (47.38%), Space: 42.2 MB (58.79%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,31 +1,28 @@
 class Solution {
-    boolean[][] visited;
     public boolean exist(char[][] board, String word) {
-        visited = new boolean[board.length][board[0].length];
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(word.charAt(0)==board[i][j] && helper(board,word,i,j,0)){
+        int n = board.length;
+        int m = board[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word.charAt(0)&& helper(board,i,j,word,0)){
                     return true;
                 }
             }
         }
         return false;
+        
     }
- boolean   helper(char[][] board , String word , int i,int j,int index){
+    boolean helper(char[][] board,int i,int j , String word , int index){
         if(index==word.length()) return true;
+        if(i<0||j<0||i>=board.length||j>=board[0].length||board[i][j]!=word.charAt(index)) return false;
         
-        if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word.charAt(index) || visited[i][j]){
-            return false;
-        }
-        
-        visited[i][j] = true;
-        if(helper(board, word, i-1, j, index+1) || 
-           helper(board, word, i+1, j, index+1) ||
-           helper(board, word, i, j-1, index+1) || 
-           helper(board, word, i, j+1, index+1)){
-            return true;
-        }
-     visited[i][j] = false;
-     return false;
-}
+        char temp = board[i][j];
+        board[i][j] = '!';
+        boolean duplicate = helper(board,i+1,j,word,index+1)||
+            helper(board,i-1,j,word,index+1)||
+            helper(board,i,j+1,word,index+1)||
+            helper(board,i,j-1,word,index+1);
+        board[i][j] = temp;
+        return duplicate;
+    }
 }
