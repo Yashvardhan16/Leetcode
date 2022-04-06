f55263855e24848982c33e5e353fb2dc88027de3
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-04-06 12:14:19 +0530

    Time: 21 ms (27.98%), Space: 70.3 MB (36.22%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,23 +1,22 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // Map<Integer,Integer> map = new HashMap<>();
-        // for(int i:nums){
-        //     map.put(i,map.getOrDefault(i,0)+1);
-        // }
-        // for(int i=0;i<nums.length;i++){
-        //     if( map.get(nums[i])>1){
-        //        return true;
-        //     }
-        // }
-        // return false;
-       Set<Integer> set = new HashSet();
+        Map<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            if(set.contains(nums[i])){
-                return true;
+            if( map.containsKey(nums[i])){
+               return true;
             }else{
-                set.add(nums[i]);
+                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
             }
         }
         return false;
+       // Set<Integer> set = new HashSet();
+       //  for(int i=0;i<nums.length;i++){
+       //      if(set.contains(nums[i])){
+       //          return true;
+       //      }else{
+       //          set.add(nums[i]);
+       //      }
+       //  }
+       //  return false;
     }
 }
