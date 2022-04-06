bc0dd1bbfb8282dc9d1bcdb64ac30bffad569a45
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-04-06 12:09:19 +0530

    Time: 21 ms (27.98%), Space: 52.2 MB (97.11%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -4,9 +4,8 @@ class Solution {
         for(int i:nums){
             map.put(i,map.getOrDefault(i,0)+1);
         }
-        int count =0;
         for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i]) && map.get(nums[i])>1){
+            if( map.get(nums[i])>1){
                return true;
             }
         }
