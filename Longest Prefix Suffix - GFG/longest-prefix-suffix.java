acd309efc58f929b8af4a787262fea1b18c8f992
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-04-08 12:52:51 +0530

    Added solution - LeetHub

---
+++ Longest Prefix Suffix - GFG/longest-prefix-suffix.java
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            
+            String s = read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.lps(s));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int lps(String s) {
+        // code here
+        int n = s.length();
+        int len =0;
+        int[] lps = new int[n];
+        lps[0] =0;
+        int i=1;
+        while(i<n){
+            if(s.charAt(i)==s.charAt(len)){
+                len++;
+                lps[i] = len;
+                i++;
+            }else if(len==0){
+                lps[i] =0;
+                i++;
+            }else{
+                len = lps[len-1];
+            }
+        }
+        return len;
+    }
+}
