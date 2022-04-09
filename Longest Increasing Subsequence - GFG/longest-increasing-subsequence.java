ae5bad9204fc24505e102c84cf63363c1e2d1288
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-04-09 17:13:18 +0530

    Added solution - LeetHub

---
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.java
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        
+        //taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total testcases
+        int t = sc.nextInt();
+        while(t > 0){
+            
+            //taking size of array
+            int n = sc.nextInt();
+            int array[] = new int[n];
+            
+            //inserting elements in the array
+            for (int i = 0; i < n; ++i)
+            {
+                array[i] = sc.nextInt();
+            }
+            
+            //creating an object of class Solution
+            Solution ob = new Solution();
+            
+            //calling longestSubsequence() method of class
+            //Solution
+            System.out.println(ob.longestSubsequence(n,array));
+            t--;
+        }
+    } 
+} // } Driver Code Ends
+
+
+
+
+class Solution 
+{
+    //Function to find length of longest increasing subsequence.
+    static int longestSubsequence(int size, int a[])
+    {
+        // code here
+        int[] dp = new int[size+1];
+        Arrays.fill(dp,1);
+        int max_len=0;
+        for(int i=0;i<size;i++){
+            for(int j=0;j<i;j++){
+            if(a[i]>a[j]){
+                dp[i] = Math.max(dp[i],dp[j]+1);
+            }
+            max_len = Math.max(max_len,dp[i]);
+        }
+         
+        }
+        return max_len;
+    }
+} 
