df656e5c95263ef10c697fecffb09bffafe46b2e
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-04-11 11:52:39 +0530

    Time: 16 ms (22.88%), Space: 48 MB (13.21%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,30 +1,58 @@
 class Solution {
-//     Map<String,Boolean> map = new HashMap<>();
-//     public boolean wordBreak(String s, List<String> wordDict) {
-//         if(wordDict.contains(s)) return true;
-//         if(map.containsKey(s)) return map.get(s);
-        
-//         for(int i=1;i<=s.length();i++){
-//             String left = s.substring(0,i);
-//             if(wordDict.contains(left) && wordBreak(s.substring(i),wordDict)){
-//                 map.put(s,true);
-//                 return true;
-//             }
-//         }
-//         map.put(s,false);
-//         return false;
-    public boolean wordBreak(String s, List<String> wordDict){
-        boolean[] dp = new boolean[s.length()+1];
+    // HashMap
+    Map<String,Boolean> map = new HashMap<>();
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if(wordDict.contains(s)) return true;
+        if(map.containsKey(s)) return map.get(s);
         Set<String> set = new HashSet(wordDict);
-        dp[0] = true;
         for(int i=1;i<=s.length();i++){
-            for(int j=0;j<=i;j++){
-                if(dp[j] && set.contains(s.substring(j,i))){
-                    dp[i] = true;
-                break;
-                }
+            String left = s.substring(0,i);
+      if(set.contains(left) && wordBreak(s.substring(i),wordDict)){
+                map.put(s,true);
+                return true;
             }
         }
-     return dp[s.length()];
+        map.put(s,false);
+        return false;
     }
 }
+    //dp solution
+    // public boolean wordBreak(String s, List<String> wordDict){
+     //    boolean[] dp = new boolean[s.length()+1];
+     //    Set<String> set = new HashSet(wordDict);
+     //    dp[0] = true;
+     //    for(int i=1;i<=s.length();i++){
+     //        for(int j=0;j<=i;j++){
+     //            //set for improving the time complexity
+     //            //dp[j] is the string covered so far
+     //            if(dp[j] && set.contains(s.substring(j,i))){
+     //                dp[i] = true;
+     //            break;
+     //            }
+     //        }
+     //    }
+     // return dp[s.length()];
+        
+    //BackTracking
+//     public boolean wordBreak(String s, List<String> wordDict) {
+//   
+//   return backtracking(0, s, new HashSet<>(wordDict));
+// }
+
+// private boolean backtracking(int depth, String s, Set<String> wordSet) {
+//   int n = s.length();
+//   // accept
+//   if (depth == n) {
+//     return true;
+//   }
+  
+//   for (int i = depth; i < n; ++i) {
+//     String str = s.substring(depth, i + 1); // substring[depth, i]
+//     if (wordSet.contains(str)) {
+//       if (backtracking(i + 1, s, wordSet)) return true;
+//     }
+//   }
+  
+//   return false;
+// }
+//     }
