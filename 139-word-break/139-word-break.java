9317b36f96c4cd96e1dfde73af83172ccc76f6f5
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-04-11 11:59:12 +0530

    Time: 13 ms (39.61%), Space: 48.1 MB (11.52%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -4,8 +4,9 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         if(wordDict.contains(s)) return true;
         if(map.containsKey(s)) return map.get(s);
+        //set for the sole purpose of time complexity
         Set<String> set = new HashSet(wordDict);
-        for(int i=1;i<=s.length();i++){
+        for(int i=0;i<s.length();i++){
             String left = s.substring(0,i);
       if(set.contains(left) && wordBreak(s.substring(i),wordDict)){
                 map.put(s,true);
