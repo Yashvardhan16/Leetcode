ff4c4cf0e402e5213621301156f7def1e75d28ca
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-04-11 11:42:57 +0530

    Time: 20 ms (11.69%), Space: 46.7 MB (53.83%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,17 +1,30 @@
 class Solution {
-    Map<String,Boolean> map = new HashMap<>();
-    public boolean wordBreak(String s, List<String> wordDict) {
-        if(wordDict.contains(s)) return true;
-        if(map.containsKey(s)) return map.get(s);
+//     Map<String,Boolean> map = new HashMap<>();
+//     public boolean wordBreak(String s, List<String> wordDict) {
+//         if(wordDict.contains(s)) return true;
+//         if(map.containsKey(s)) return map.get(s);
         
+//         for(int i=1;i<=s.length();i++){
+//             String left = s.substring(0,i);
+//             if(wordDict.contains(left) && wordBreak(s.substring(i),wordDict)){
+//                 map.put(s,true);
+//                 return true;
+//             }
+//         }
+//         map.put(s,false);
+//         return false;
+    public boolean wordBreak(String s, List<String> wordDict){
+        boolean[] dp = new boolean[s.length()+1];
+        Set<String> set = new HashSet(wordDict);
+        dp[0] = true;
         for(int i=1;i<=s.length();i++){
-            String left = s.substring(0,i);
-            if(wordDict.contains(left) && wordBreak(s.substring(i),wordDict)){
-                map.put(s,true);
-                return true;
+            for(int j=0;j<=i;j++){
+                if(dp[j] && wordDict.contains(s.substring(j,i))){
+                    dp[i] = true;
+                break;
+                }
             }
         }
-        map.put(s,false);
-        return false;
+     return dp[s.length()];
     }
 }
