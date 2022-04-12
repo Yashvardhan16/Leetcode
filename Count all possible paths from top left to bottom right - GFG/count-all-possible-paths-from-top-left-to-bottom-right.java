8a7163a7e42dcb23cca1e7ebbc2eef142624cf4a
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-04-12 13:16:31 +0530

    Added solution - LeetHub

---
+++ Count all possible paths from top left to bottom right - GFG/count-all-possible-paths-from-top-left-to-bottom-right.java
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] input = new String[2]; 
+            input = br.readLine().split(" "); 
+            int m = Integer.parseInt(input[0]); 
+            int n = Integer.parseInt(input[1]); 
+            Solution ob = new Solution();
+            System.out.println(ob.numberOfPaths(m,n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    long numberOfPaths(int m, int n)
+    {
+        // code here
+        long mod= 1000000007;
+        long[][] dp = new long[m][n];
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0||j==0){
+                    dp[i][j]=1;
+                }else{
+                    dp[i][j] = dp[i][j-1]%mod + dp[i-1][j]%mod;
+                }
+            }
+        }
+        return dp[m-1][n-1]%mod;
+    }
+}
