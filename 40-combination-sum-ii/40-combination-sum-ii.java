dbe754caec1c3ed366768cd70fb8a64e325ba81c
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-04-13 11:18:44 +0530

    Time: 3 ms (93.24%), Space: 42.9 MB (81.26%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,20 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-          List<List<Integer>> list = new LinkedList<>();
-        helper(candidates,0,new ArrayList<>(),target,list);
+        List<List<Integer>> list = new LinkedList<>();
+     backtrack( candidates,0,new ArrayList(),target,list);
         return list;
     }
-   void helper(int[] candidates,int start,List<Integer> ans,int target,List<List<Integer>> list){
-         if(target<0) return;
-       if(target==0)  list.add(new ArrayList<>(ans));
-      
-        for(int i=start;i<candidates.length;i++){
-            if(i > start && candidates[i] == candidates[i-1]) continue;
-            if(target-candidates[i] < 0) break;
-            ans.add(candidates[i]);
-            helper(candidates,i+1,ans,target-candidates[i],list);
-            ans.remove(ans.size()-1);
-        }
+    
+ void backtrack(int[] candidates,int start,List<Integer> ans,int target,List<List<Integer>> list){
+        if(target<0) return;
+       if(target==0){
+           list.add(new ArrayList<>(ans));
+       }
+       for(int i =start;i<candidates.length;i++){
+           if(i>start && candidates[i]==candidates[i-1])
+               continue;
+           if(target-candidates[i]<0) break;
+           ans.add(candidates[i]);
+           backtrack(candidates,i+1,ans,target -candidates[i],list);
+           ans.remove(ans.size()-1);
+       }
     }
 }
