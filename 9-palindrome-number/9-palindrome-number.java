4980aaacfda92a233da4eaf7ca885696e7b51ef9
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-04-13 11:04:31 +0530

    Time: 19 ms (20.09%), Space: 41.6 MB (91.21%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,26 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) return false;
-
-	//stack for digits
-	Stack<Integer> stack = new Stack();
-
-	//fill stack w/ digits from right to left
-	int n = x;
-	while (n > 0) {
-		//push digit to stack
-		stack.push(n % 10);
-		n /= 10;
-	}
-
-	n = x;
-	while (n > 0) {
-		//check in reverse order from stack to see if digits match
-		if (n % 10 != stack.pop()) return false;
-		n /= 10;
-	}
-
-	//get here its true
-	return true;
+        if(x<0) return false;
+        Stack<Integer> st = new Stack();
+              int temp = x;
+        while(temp>0){
+            st.push(temp%10);
+            temp/=10;
+        }
+        temp = x;
+        while(temp>0){
+            if(temp%10!=st.pop())
+                return false;
+            temp/=10;
+        }
+        return true;
     }
 }
