23eba30444abe687ea2b4262024dccefa3e77a4e
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-04-13 11:06:22 +0530

    Time: 9 ms (84.73%), Space: 42.1 MB (83.45%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,18 +1,14 @@
 class Solution {
-    public boolean isPalindrome(int x) {
-        if(x<0) return false;
-        Stack<Integer> st = new Stack();
-              int temp = x;
-        while(temp>0){
-            st.push(temp%10);
-            temp/=10;
+    public boolean isPalindrome(int n) {
+     int ans=0;
+        int temp=n;
+        while(n>0){
+            ans=ans*10+n%10;
+            n/=10;
         }
-        temp = x;
-        while(temp>0){
-            if(temp%10!=st.pop())
-                return false;
-            temp/=10;
+        if(ans==temp){
+            return true;
         }
-        return true;
+        return false;
     }
 }
