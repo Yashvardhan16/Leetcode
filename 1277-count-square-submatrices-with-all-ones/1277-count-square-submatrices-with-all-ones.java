22175375b328c817f79936ac7a934db3e262d9fb
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-05-24 10:00:42 +0530

    Time: 9 ms (37.09%), Space: 68.5 MB (62.05%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -15,7 +15,7 @@ matrix[i][j] = Math.min(matrix[i-1][j],Math.min(matrix[i-1][j-1],matrix[i][j-1])
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 
-                count[matrix[i][j]] =count[matrix[i][j]] +matrix[i][j]; 
+                count[matrix[i][j]] +=matrix[i][j]; 
             }
         }
         for(int i=1;i<count.length;i++){
