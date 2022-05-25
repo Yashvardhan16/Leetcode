3641211a6bc84ee000f44158180db6a327a2ed2e
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-05-25 13:57:28 +0530

    Time: 17 ms (18.61%), Space: 43.2 MB (41.40%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.java
+++ 16-3sum-closest/16-3sum-closest.java
@@ -9,8 +9,15 @@ class Solution {
             int k = n-1;
             while(j<k){
                 int currsum = nums[i]+nums[j]+nums[k];
-                if(currsum>target) k--;
-                else  j++;
+                if(currsum>target){ 
+                while(j<k && nums[k] == nums[k-1]){
+                        k--;
+                    }
+                       k--;           }
+                else  { while(j<k && nums[j+1] == nums[j]){
+                        j++;
+                    } j++;
+                      }
                 if(Math.abs(sum-target)>Math.abs(currsum-target)){
                     sum=currsum;
                 }
