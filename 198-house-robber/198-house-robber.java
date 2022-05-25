a0d5a1b29c1760890a5d48a7b12b5d0e46e96604
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-05-25 14:17:12 +0530

    Time: 1 ms (17.26%), Space: 42.1 MB (15.51%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,13 +1,13 @@
 class Solution {
     public int rob(int[] nums) {
+        if(nums.length==0) return 0;
         int n = nums.length;
-        if(n==0) return 0;
         int[] dp = new int[n+1];
-        dp[0] =0;
+         dp[0] = 0;
         dp[1] = nums[0];
-        
+     
         for(int i=1;i<n;i++){
-            dp[i+1] = Math.max(dp[i],dp[i-1]+nums[i]);
+            dp[i+1] = Math.max(dp[i],dp[i-1]+nums[i]); 
         }
         return dp[n];
     }
