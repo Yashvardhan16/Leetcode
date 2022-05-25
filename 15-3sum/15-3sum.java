ac0bd984d9d582ca12f3740b9719987ba1a6ee44
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-05-25 13:42:49 +0530

    Time: 104 ms (30.76%), Space: 65.4 MB (21.36%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -10,7 +10,10 @@ class Solution {
                 if(sum==-nums[i]){
                     set.add(Arrays.asList(nums[i],nums[j],nums[k]));
                     j++;k--;
-                }else if(sum>-nums[i]){
+                       while (j < k && nums[j] == nums[j - 1]) j++; 
+                while (j < k && nums[k] == nums[k + 1]) k--;  
+                }
+                else if(sum>-nums[i]){
                     k--;
                 }else{
                     j++;
