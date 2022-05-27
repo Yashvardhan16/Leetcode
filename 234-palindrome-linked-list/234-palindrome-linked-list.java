05cc2c922f339d8f0c6f8f722fbce60c977d60c9
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-05-27 15:21:46 +0530

    Time: 36 ms (11.53%), Space: 59.2 MB (77.78%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -10,18 +10,18 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode copy = head;
+        ListNode curr = head;
         Stack<Integer> st = new Stack();
-        while(copy!=null){
-            st.push(copy.val);
-            copy = copy.next;
-             } 
-            while(!st.isEmpty()){
-                if(st.pop()!=head.val)
+        while(curr!=null){
+            st.push(curr.val);
+            curr = curr.next;
+        }
+        while(!st.isEmpty()){
+            if(st.pop()!=head.val)
                 return false;
+                head = head.next;
             
-            head = head.next;
-    }
+        }
         return true;
     }
 }
