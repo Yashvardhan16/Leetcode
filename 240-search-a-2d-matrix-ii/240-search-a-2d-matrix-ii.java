99cea9d3bcd99eb571e5d2a02e79d0b9fd51bc5a
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-05-27 16:11:17 +0530

    Time: 5 ms (95.61%), Space: 48.5 MB (80.06%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,13 +1,9 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-         if (matrix.length == 0 || matrix[0].length == 0) {
-            return false;
-        }
-        int n = matrix.length;
+        int n =matrix.length;
         int m = matrix[0].length;
-        int i=0;
-        int j = matrix[0].length-1;
-        while(i<matrix.length && j>=0){
+        int i=0,j=m-1;
+        while(i<n && j>=0){
             if(matrix[i][j]==target){
                 return true;
             }
@@ -17,6 +13,6 @@ class Solution {
                 i++;
             }
         }
-      return false;
+        return false;
     }
 }
