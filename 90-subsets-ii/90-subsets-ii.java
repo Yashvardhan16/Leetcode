5d2651b309291e149ced14a51794957da870b9cd
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-05-28 20:41:08 +0530

    Time: 2 ms (76.18%), Space: 44.5 MB (17.10%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,20 +1,18 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> list  = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> list = new ArrayList<>();
-        if(nums==null) return list;
-        helper(nums,0,new ArrayList<>(),list);
+        helper(nums,0,list,new ArrayList());
         return list;
     }
-    void helper(int[] nums,int start,List<Integer> ans, List<List<Integer>> list){
-        list.add(new ArrayList<>(ans));
+    void helper(int[] nums,int start, List<List<Integer>> list ,List<Integer> res){
+        list.add(new ArrayList(res));
         
         for(int i=start;i<nums.length;i++){
-             if(i>start&&nums[i]==nums[i-1]) continue;
-            ans.add(nums[i]);
-            helper(nums,i+1,ans,list);
-            ans.remove(ans.size()-1);
-            
+            if(i>start && nums[i]==nums[i-1]) continue;
+            res.add(nums[i]);
+            helper(nums,i+1,list,res);
+            res.remove(res.size()-1);
         }
     }
 }
