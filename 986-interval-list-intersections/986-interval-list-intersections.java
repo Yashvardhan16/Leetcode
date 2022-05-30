e857d75a4a564ccd896cd14a2fd4884117b21d85
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-05-30 10:06:07 +0530

    Time: 5 ms (44.96%), Space: 55.1 MB (19.66%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.java
+++ 986-interval-list-intersections/986-interval-list-intersections.java
@@ -1,23 +1,24 @@
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
         List<int[]> list = new ArrayList<>();
-        int ptr1 =0 , ptr2=0;
+        int ptr1=0;
+        int ptr2=0;
+        
         while(ptr1<firstList.length && ptr2<secondList.length){
-            //csp  = common starting point
-            //cep = common ending point
+            
+        //csp = common starting point , cep = common ending point
         int csp = Math.max(firstList[ptr1][0],secondList[ptr2][0]);
-      int cep = Math.min(firstList[ptr1][1],secondList[ptr2][1]);
+        int cep =  Math.min(firstList[ptr1][1],secondList[ptr2][1]);
             
             if(csp<=cep){
-                int[] interval = {csp,cep};
+                int[] interval ={csp,cep};
                 list.add(interval);
             }
              if(firstList[ptr1][1]<secondList[ptr2][1]){
-                 ptr1++;
-             }   else{
-                 ptr2++;
-             }
-                
+                ptr1++;
+            }else{
+                ptr2++;
+            }
         }
         return list.toArray(new int[list.size()][]);
     }
