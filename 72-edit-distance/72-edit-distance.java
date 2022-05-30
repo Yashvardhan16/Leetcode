f57c4747a7199fb80b1e8ad68315dacc4bd79432
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-05-30 14:31:03 +0530

    Time: 11 ms (22.85%), Space: 45.4 MB (19.39%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -5,9 +5,9 @@ class Solution {
         for(int i=0;i<=word1.length();i++){
             for(int j=0;j<=word2.length();j++){
                 if(i==0){
-                    dp[i][j] =j;
+                    dp[i][j] = j;
                 }else if(j==0){
-                    dp[i][j] = i;
+                    dp[i][j] =i;
                 }else if(word1.charAt(i-1)==word2.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1];
                 }else{
