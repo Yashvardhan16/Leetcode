105324263db3537ac5e132f4cd2e090ffc06080b
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-05-30 21:32:36 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (48.21%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,19 +1,14 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        // using lo and hi pointer find the mid element
-        //compare mid element to mid-1 & mid+1
-        //if mid element bigger return mid
-        
-        int n = nums.length;
-        int lo=0;int hi = n-1;
+        int lo=0;
+        int hi = nums.length-1;
         while(lo<hi){
             int mid = lo +(hi-lo)/2;
-           if(nums[mid]<nums[mid+1]){
-                lo =mid+1;
-           }
-            else{
-                hi=mid;
+            
+            if(nums[mid]<nums[mid+1]){
+                lo = mid+1;
             }
+            else hi =mid;
         }
         return lo;
     }
