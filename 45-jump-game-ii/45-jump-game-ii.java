b86d87f36e3c09595b46d173c9014f3899273e17
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-05-30 11:05:24 +0530

    Time: 3 ms (52.41%), Space: 49.1 MB (63.16%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,16 +1,14 @@
 class Solution {
     public int jump(int[] nums) {
-        int n = nums.length;
-        int last=0;
-        int curr=0;
+        int steps =0;
         int count =0;
-        for(int i=0;i<n-1;i++){
-            curr = Math.max(curr,nums[i]+i);
+        int last=0;
+        for(int i=0;i<nums.length-1;i++){
+            steps = Math.max(steps,nums[i]+i);
             
-            if(i==last){
-                count++;
-                
-                last=curr;
+            if(last==i){
+               count++;
+                last=steps;
             }
         }
         return count;
