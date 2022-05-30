841b6c2d425f4705af5c7b4da53625ab45dff005
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-05-30 13:57:16 +0530

    Time: 28 ms (48.70%), Space: 43.9 MB (68.76%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -1,50 +1,45 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums);
         int n = nums.length;
+        Arrays.sort(nums);
         
-        for(int i=0;i<n-3;i++){
-            //removing duplicates(same value)
-            if(i!=0 && nums[i]==nums[i-1]) continue;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+        
+        for(int j=i+1;j<n;j++){
+            if(j>i+1 && nums[j]==nums[j-1]) continue;
             
-            for(int j=i+1;j<n-2;j++){
-                //removing duplicates(same value)
-                if(j!=i+1 && nums[j]==nums[j-1]) continue;
-                
-                int left = j+1;
-                int right = nums.length-1;
-                
-                while(left<right){
-                 int sum = nums[i]+ nums[j] + nums[left]+nums[right];
-                    
-                    if(sum<target){
-                        left++;
-                    }else if(sum>target){
-                        right--;
-                    }
-                    //sum=target case
-                    else{
-                        List<Integer> res = new ArrayList();
-                        res.add(nums[i]);
-                        res.add(nums[j]);
-                        res.add(nums[left]);
-                        res.add(nums[right]);
-                        list.add(res);
-                        left++;right--;
-                    
-                    //removing duplicates
-                    while(left<right && nums[left]==nums[left-1]){
-                        left++;
-                    }
-                     while(left<right && nums[right]==nums[right+1]){
-                        right--;
-                    }
-                }
+            int left = j+1;
+            int right = n-1;
+            while(left<right){
+            int sum = nums[i]+nums[j]+nums[left]+nums[right];
+            if(sum>target){
+                right--;
+            }else if(sum<target){
+                left++;
             }
+            
+          else{
+              List<Integer> res = new ArrayList();
+              res.add(nums[i]);
+               res.add(nums[j]);
+               res.add(nums[left]);
+               res.add(nums[right]);
+              list.add(res);
+              left++;
+              right--;
+              
+              while(left<right && nums[left] ==nums[left-1]){
+                  left++;
+              }
+              while(left<right && nums[right] ==nums[right+1]){
+                  right--;
+              }
+          }
+        }
         }
         }
         return list;
-        
     }
 }
