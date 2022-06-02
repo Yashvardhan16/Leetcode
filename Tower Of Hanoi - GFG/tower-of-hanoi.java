6b3d95f9bfb6f58589fc129303da1f918c11a9fa
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-06-02 11:28:36 +0530

    Added solution - LeetHub

---
+++ Tower Of Hanoi - GFG/tower-of-hanoi.java
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class Recursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();//total testcases
+        while (T-- > 0) {
+            Hanoi obj = new Hanoi();
+            int N;
+            
+            //taking input N
+            N = sc.nextInt();
+
+            //calling toh() method 
+            System.out.println(obj.toh(N, 1, 3, 2));
+        }
+    }
+}
+
+
+
+
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+
+// avoid space at the starting of the string in "move disk....."
+class Hanoi {
+
+    public long toh(int N, int from, int to, int aux) {
+        // Your code here
+        helper(N,from,to,aux);
+        return (long)Math.pow(2,N)-1;
+    }
+    void helper(int N, int from, int to, int aux){
+        if(N==0) return;
+        
+        helper(N-1,from,aux,to);
+        System.out.println("move disk "+N+" from rod "+from+" to rod "+to);
+        helper(N-1,aux,to,from);
+    }
+}
