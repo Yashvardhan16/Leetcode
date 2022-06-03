1153439203e46360f99ff888b31503e8096f59e7
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-06-03 09:55:02 +0530

    Time: 6 ms (87.67%), Space: 50.7 MB (95.78%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,26 +1,17 @@
 class Solution {
-    public int countSquares(int[][] matrix) {
-        if(matrix.length==0) return 0;
+    public int countSquares(int[][] matrix) {        if(matrix.length==0||matrix[0].length==0) return 0;
         int ans =0;
-        int total =0;
-        for(int i=1;i<matrix.length;i++){
-            for(int j=1;j<matrix[0].length;j++){
-                if(matrix[i][j]==1){
-matrix[i][j] = Math.min(matrix[i-1][j],Math.min(matrix[i-1][j-1],matrix[i][j-1]))+1;
-                    ans = Math.max(ans,matrix[i][j]);
-                }
-            }
-        }
-        int[] count = new int[ans+1];
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-                
-                count[matrix[i][j]] +=matrix[i][j]; 
+                if(i==0||j==0){
+                    ans+=0;
+                }
+                else if(matrix[i][j]>0 && i>0 && j>0){
+                    matrix[i][j]  = Math.min(matrix[i-1][j-1],Math.min(matrix[i-1][j],matrix[i][j-1]))+1;
+                }
+                ans += matrix[i][j];
             }
         }
-        for(int i=1;i<count.length;i++){
-            total += count[i];
+             return   ans;
+            }
         }
-        return total;
-    }
-}
