9face06832b1b84c5118cf4d6355ac2bf2f527cd
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-06-10 15:13:51 +0530

    Added solution - LeetHub

---
+++ Anagram Palindrome - GFG/anagram-palindrome.java
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG
+{
+    public static void main (String[] args)
+    {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for(int i=0;i<t;i++){
+		    String s = in.next();
+		    if (new Sol().isPossible (s) == 1)
+		        System.out.println ("Yes");
+		    else
+		        System.out.println ("No");
+		}
+	}
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
+
+
+class Sol
+{
+    int isPossible (String s)
+    {
+        // your code here
+         if(s==null||s.length()==0) return 1;
+        Map<Character,Integer> map = new HashMap<>();
+
+        for(char c: s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        int count =0;
+        for(char i: map.keySet()){
+            if(map.get(i)%2==1){
+                count++;
+            }
+        }
+        return count<=1?1:0;
+    }
+}
